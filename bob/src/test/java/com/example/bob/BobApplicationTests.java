a1253e1a416658fca853a022f52666a941ab3e35
콩dori <coalsdlzhdwk@naver.com>
2024-12-23 00:43:52 +0900

    작업을 위한 각 종 패키지 및 폴더 생성

---
+++ bob/src/test/java/com/example/bob/BobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
