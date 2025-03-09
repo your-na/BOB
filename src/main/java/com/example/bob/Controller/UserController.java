4afc2a09c0f0d31565cdd51f7c41df9337803b50
(유나)DESKTOP-VVFOHLM\강보원 <wannaturi@gmail.com>
2025-03-09 23:34:03 +0900

    ,공모전 상세 ( 즐겨찾기 추후 수정), /postcontest 유저 컨트롤러 내에 임의 등록

--- src/main/java/com/example/bob/Controller/UserController.java
+++ src/main/java/com/example/bob/Controller/UserController.java
@@ -154,4 +154,6 @@ public class UserController {
     //공모전 임의 주소 여기다 할 예정
     @GetMapping("/contest")
     public String contestform() {return "contest";}
+    @GetMapping("/postcontest")
+    public String postcform() {return "postcontest";}
 }
