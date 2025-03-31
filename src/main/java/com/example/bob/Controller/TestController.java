fa206ccf806adb82c6411a688a1b84d035ac94cd
(유나)DESKTOP-VVFOHLM\강보원 <wannaturi@gmail.com>
2025-03-31 15:54:45 +0900

    구직공고 내일 다시 이력서 업로드 부분 수정할 예정

--- src/main/java/com/example/bob/Controller/TestController.java
+++ src/main/java/com/example/bob/Controller/TestController.java
@@ -11,6 +11,10 @@ public class TestController {
     public String newcontestform() {return "co_resume";}
 
     //기업측 이력서 목록
-    @GetMapping("coresumelist")
+    @GetMapping("/coresumelist")
     public String resumelistform() {return "co_resumelist";}
+
+    //구직 공고 상세
+    @GetMapping("/jobindex")
+    public String jobinform() {return "jobindex";}
 }
