d475e5fe84385d669e1d25060d17bd76ac49720d
콩dori <coalsdlzhdwk@naver.com>
2025-09-14 06:09:20 +0900

    불합격 구현

---
+++ src/main/java/com/example/bob/DTO/MyResumeApplicationRequest.java
@@ -0,0 +1,12 @@
+package com.example.bob.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MyResumeApplicationRequest {
+    private Long jobPostId;   // 지원한 공고 ID
+    private Long myResumeId;   // 나만의 이력서 ID (MyResume의 id)
+    private String message;   // 합격 메시지
+}
