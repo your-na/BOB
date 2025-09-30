0510cb414e3025a6ab7454601a2d9ae89aec8248
(유나)DESKTOP-VVFOHLM\강보원 <wannaturi@gmail.com>
2025-09-30 12:48:41 +0900

    충돌 해결

--- src/main/resources/static/js/todo_plan2.js
+++ src/main/resources/static/js/todo_plan2.js
@@ -23,11 +23,8 @@ document.addEventListener("DOMContentLoaded", function () {
             if (text === "홈") {
                 window.location.href = `/contesthome/${teamId}`;
             } else if (text === "WBS") {
-<<<<<<< HEAD
-                window.location.href = `/todocrud//contest/${teamId}`;
-=======
+
                 window.location.href = `/todocrud/contest/${teamId}`;
->>>>>>> kbw
             }
         });
     });
